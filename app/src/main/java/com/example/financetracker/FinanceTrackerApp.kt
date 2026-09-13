d0f61ef9e5900cad0a72b02eee97f4d9c6813915
pikselvorkeraund <pikselvorkeraund@gmail.com>
2026-09-13 20:16:28 +0300

    Update FinanceTrackerApp.kt

--- app/src/main/java/com/example/financetracker/FinanceTrackerApp.kt
+++ app/src/main/java/com/example/financetracker/FinanceTrackerApp.kt
@@ -1,11 +1,7 @@
-﻿package com.example.financetracker
+package com.example.financetracker
+
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import net.sqlcipher.database.SQLiteDatabase
+
 @HiltAndroidApp
-class FinanceTrackerApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        SQLiteDatabase.loadLibs(this)
-    }
-}
+class FinanceTrackerApp : Application()
